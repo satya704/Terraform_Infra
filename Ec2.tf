b60007654436c0223264653e15f007c26c506757
satyagsn704 <satyagsn704@gmail.com>
2024-01-10 17:32:38 +0530

    modefied the ec2 instance type

--- Ec2.tf
+++ Ec2.tf
@@ -3,7 +3,7 @@ module "ec2_instance" {
 
   name = "single-instance"
 
-  instance_type          = "t2.micro"
+  instance_type          = "t2.medium"
   key_name               = "latest_key"
   monitoring             = true
   vpc_security_group_ids = ["sg-0228d1e3fdffc59ef"]
