59ad889fae7afbd87baeffd7dd30dd3cccade2a5
satyagsn704 <satyagsn704@gmail.com>
2024-01-10 18:25:27 +0530

    chagned the html code

--- Ec2.tf
+++ Ec2.tf
@@ -21,7 +21,7 @@ module "ec2_instance" {
         echo '<!DOCTYPE html>' > /var/www/html/index.html
         echo '<html lang="en">' >> /var/www/html/index.html
         echo '<body style="background-color:black;">' >> /var/www/html/index.html
-        echo '  <h1 style="color:red;"> Welcome To Microland: HTTP access successfully configured!</h1>' >> /var/www/html/index.html
+        echo '  <h1 style="color:red;"> Welcome To Microland DevOps team !</h1>' >> /var/www/html/index.html
         echo '</body>' >> /var/www/html/index.html
         echo '</html>' >> /var/www/html/index.html
 	EOF
