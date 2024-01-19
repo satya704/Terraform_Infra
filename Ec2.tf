41e2dda28f19459a969b45508eb472c7adca4d69
satyagsn704 <satyagsn704@gmail.com>
2024-01-19 12:54:17 +0530

    removed the user data in Ec2 instance

--- Ec2.tf
+++ Ec2.tf
@@ -12,17 +12,4 @@ module "ec2_instance" {
     Terraform   = "true"
     Environment = "dev"
   }
-  user_data = <<EOF
-        #!/bin/bash
-        sudo yum update -y
-        sudo yum install httpd.x86_64 -y
-        systemctl restart httpd.service
-        systemctl enable httpd.service
-        echo '<!DOCTYPE html>' > /var/www/html/index.html
-        echo '<html lang="en">' >> /var/www/html/index.html
-        echo '<body style="background-color:black;">' >> /var/www/html/index.html
-        echo '  <h1 style="color:red;"> Welcome To Microland DevOps team !</h1>' >> /var/www/html/index.html
-        echo '</body>' >> /var/www/html/index.html
-        echo '</html>' >> /var/www/html/index.html
-	EOF
 }
